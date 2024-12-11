f949babed9a31a97cfe24531a580d774ea0f0985
Ricardo Nascimento <rncnascimento@gmail.com>
2024-12-11 09:54:28 +0000

    meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { KasadaClient } from "@/utils/kasada/kasada-client";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://chat.vercel.ai"),
-  title: "Next.js Chatbot Template",
-  description: "Next.js chatbot template using the AI SDK.",
+  title: "Assistente RH - Made2Web",
+  description: "Assistente RH - Made2Web",
 };
 
 export default async function RootLayout({
